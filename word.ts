8443b15f5bdb40d924752e82a950ca847d7d743c
Iqra Jawed <162354207+iqrajawed@users.noreply.github.com>
2024-05-24 12:37:32 +0500

    word counter project

---
+++ word.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from'inquirer';
+import chalk from 'chalk';
+
+const answers:{
+    sentence:string} = await inquirer.prompt([
+        {
+        name:'sentence',
+        type:'input',
+        message: chalk.red('\n \t Enter your sentences ')
+            
+        }
+])
+const words = answers.sentence.trim().split(" ");
+console.log( words);
+console.log(`\n \t ${words}`);
+
+console.log(chalk.yellow
+ (`\n \t Your sentence count the word is ${words.length}`));
+
+
+
